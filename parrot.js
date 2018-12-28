138815eaedff460e1ce8e0b2ddf1e12088b6d17f
Diogo Goncalves <diogo@ticketmates.com.au>
2018-12-28 16:05:43 +1000

    First commit

---
+++ parrot.js
@@ -0,0 +1,69 @@
+class Parrot {
+	
+	constructor(options) {
+
+		this.options = options;
+		this.default = {
+			inputSelector: '.js-parrot',
+			domains: [
+				"gmail.com",
+				"hotmail.com",
+				"yahoo.com",
+				"outlook.com",
+				"live.com",
+				"icloud.com",
+				"msn.com",
+			],
+		};	
+
+		this.config = Object.assign({}, this.default, this.options);
+		this.bindInput();
+	}
+
+	get input() { return document.querySelector(this.config.inputSelector); }
+
+	bindInput() {
+		this.input.addEventListener('keyup', () => {
+			let email = this.input.value;
+			let indexOfAt = email.indexOf("@");
+			if (indexOfAt == -1) return;
+
+			let substringAt = email.substring(indexOfAt + 1)
+			if (substringAt.length == 0) return;
+
+			this.clearSuggestions();
+
+			let emailDomain = email.substring(indexOfAt + 1);
+			let domainsFound = this.config.domains.filter(domain => domain.startsWith(emailDomain));
+			if (domainsFound.length == 0) return;
+
+			let emailUsername = email.substring(0, indexOfAt);
+			let suggestions = domainsFound.map(domain => `${emailUsername}@${domain}`);
+			this.showSuggestions(suggestions);
+		});
+	}
+
+	clearSuggestions() {
+		let suggestions = document.querySelector('.parrot-suggestions');
+		if (suggestions) {
+			suggestions.parentNode.removeChild(suggestions);
+		}
+	}
+
+	showSuggestions(suggestions) {
+		let suggestionsContainer = document.createElement('div');
+		suggestionsContainer.className = 'parrot-suggestions';
+		this.input.parentNode.insertBefore(suggestionsContainer, this.input.nextSibling);
+
+		suggestions.forEach(suggestion => {
+			let suggestionElement = document.createElement('div');
+			suggestionElement.className = 'parrot-suggestion';
+			suggestionElement.onclick = () => { 
+				this.input.value = suggestion 
+				this.clearSuggestions();
+			};
+			suggestionElement.textContent = suggestion;
+			suggestionsContainer.appendChild(suggestionElement);
+		});
+	}
+}
